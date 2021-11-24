6d3ce0098c4c42b6d074eb2953048e1af3942742
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2021-11-25 10:39:52 +1100

    add interface example

--- basics/Interfaces.cs
+++ basics/Interfaces.cs
@@ -1,4 +1,44 @@
-﻿public class Interfaces
+﻿public interface IChessPiece
 {
+	void Move();
+	void Capture();
+}
 
+public class Queen : IChessPiece
+{
+	public void Capture()
+	{
+		Console.WriteLine("Capture anything it can move to");
+	}
+
+	public void Move()
+	{
+		Console.WriteLine("Move horizontally, vertically, diagonally, any number of spaces");
+	}
+}
+
+public class Pawn : IChessPiece
+{
+	public void Capture()
+	{
+		Console.WriteLine("Can only capture pieces diagonally, 1 space in front of it");
+	}
+
+	public void Move()
+	{
+		Console.WriteLine("Can only move forward on space");
+	}
+
+	public void Promote()
+	{
+		Console.WriteLine("Turn into any piece at the end");
+	}
+}
+
+public class ChessGame
+{
+	void MovePiece(IChessPiece chessPiece)
+	{
+		chessPiece.Move();
+	}
 }
